15aa71c81d44df9ba15169d32806f1fe31a8d003
ypadron <ypadron82@gmail.com>
2019-02-23 23:05:08 -0500

    feat: Add requirements 2 - 2nd cat

--- js/main.js
+++ js/main.js
@@ -1,20 +1,31 @@
-const cats = {
-  name: "Ella"
-  // alt: "tabby cat in motion"
-};
-  // {name: "Legolas"}
-
+const cats = [
+  {name: "Ella"},
+  {name: "Trouble"}
+];
 
 let catName = document.getElementById("cat-name");
+let catName2 = document.getElementById("cat-name2");
 // let catName = document.querySelector(".catName");
-catName.textContent = cats.name;
+catName.textContent = cats[0].name;
+catName2.textContent = cats[1].name;
 
 let catPic = document.getElementById("cat-pic");
     catPic.setAttribute("src", "img/ella-one.jpg");
     catPic.addEventListener("click", addClick, false);
 
+let catPic2 = document.getElementById("cat-pic2");
+    catPic2.setAttribute("src", "img/trouble-one.jpg");
+    catPic2.setAttribute("alt", "white cat resting");
+    catPic2.addEventListener("click", function()  {
+      clicks2++;
+      counter2.innerHTML = clicks2;
+    }, false);
+
 let counter = document.getElementById("counter");
 let clicks = 0;
+
+let counter2 = document.getElementById("counter2");
+let clicks2 = 0;
 
 // function getCatName(cats) {
   // cats.forEach(cat => )
@@ -22,4 +33,4 @@ let clicks = 0;
 function addClick() {
   clicks++;
   counter.innerHTML = clicks;
-}
+};
