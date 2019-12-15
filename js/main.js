e58b03cccd155806abfe4385b1042b0aabdaf432
ypadron <ypadron82@gmail.com>
2019-12-14 20:05:28 -0500

    add multi cat click functions

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  self.cats = cats;
-  fillCatMenuHTML();
-});
-
+/******* model *******/
 const cats = [
-  {name: "Ella", photograph: "img/ella-one.jpg", description: "Blurry tabby cat with pink nose"},
-  {name: "Trouble", photograph: "img/trouble-one.jpg", description: "White cat resting after a hard day"},
-  {name: "Geta", photograph: "", desciption: ""},
-  {name: "Tigger", photograph: "", description: ""},
-  {name: "Indie", photograph: "img/indie-one.jpg", description: "Black cat napping in a decorative box"}
+  {name: "Ella", photograph: "img/ella-one.jpg", description: "Blurry tabby cat with pink nose", clickCount: 0},
+  {name: "Trouble", photograph: "img/trouble-one.jpg", description: "White cat resting after a hard day", clickCount: 0},
+  {name: "Greta", photograph: "img/", desciption: "", clickCount: 0},
+  {name: "Tigger", photograph: "img/", description: "", clickCount: 0},
+  {name: "Indie", photograph: "img/indie-one.jpg", description: "Black cat napping in a decorative box", clickCount: 0}
 ];
 
+/******* Octopus *******/
+function initOctopus() {
+  self.cats = cats;
+  // set currentCat, initialize cat display view
+  this.currentCat = cats[0];
+
+  // initialize cat menu view
+  fillCatMenuHTML();
+  displayCat();
+};
+
+function getCurrentCat()  {
+  return currentCat;
+};
+
+function setCurrentCat(cat)  {
+  currentCat = cat;
+};
+
+function addClick() {
+  currentCat.clickCount++;
+  renderCat();
+};
+
+/******* Cat Menu List View1 init function *******/
 let fillCatMenuHTML = (cats = self.cats) => {
-  // Find position where element will be added.
+  // Store DOM element to access later
   let catMenu = document.getElementById("cat-menu");
   cats.forEach(cat => {
     // Insert new elements (list items).
@@ -20,71 +41,49 @@ let fillCatMenuHTML = (cats = self.cats) => {
   });
 };
 
+/*** View1 - render function ***/
 let createCatMenuHTML = (cat) => {
   // Create new element and store in a variable
   let li = document.createElement("li");
-
-  // create text node and store in a variable.
+  // create text node and store in a variable
   let name = document.createElement("h3");
   name.innerHTML = cat.name;
 
-  // Attach text to the element.
+  // Attach text to the element
   li.append(name);
-  li.addEventListener("click", displayCat, false);
-  function displayCat() {
-    /** Create Display Area **/
-    let catName = document.getElementById("cat-name");
-
-    // let catName = document.querySelector(".catName");
-    catName.textContent = cat.name;
-
-    let catPic = document.getElementById("cat-pic");
-        catPic.src = cat.photograph;
-        catPic.setAttribute("alt", cat.description);
-        catPic.addEventListener("click", addClick, false);
-
-    let counter = document.getElementById("counter");
-    let clicks = 0;
-    
-    counter.textContent = clicks + " " + cat.name + " Clicks";
-
-      function addClick() {
-        clicks++;
-        counter.textContent = clicks + " " + cat.name + " Clicks";
+  // Add list item click event
+  li.addEventListener("click", (function(catCopy) {
+      return function() {
+        setCurrentCat(catCopy);
+        renderCat();
       };
-
-      // counter.textContent = clicks + " " + cat.name + " Clicks";
-
-  };
+  })(cat));
 
   return li;
 };
 
-/*
-//** Create Display Area **
-let catName = document.getElementById("cat-name");
+/** view2 - view 2 function **/
+  function displayCat() {
+    /** Create Display Area **/
+    this.displayElem = document.getElementById("display-container");
+    this.catName = document.getElementById("cat-name");
+    this.catPic = document.getElementById("cat-pic");
+    // Get DOM element for counter
+    this.counter = document.getElementById("counter");
 
-// let catName = document.querySelector(".catName");
-catName.textContent = cats[0].name;
+    this.catPic.addEventListener("click", function() {
+        addClick();
+    });
 
-let catPic = document.getElementById("cat-pic");
-    catPic.src = cats[0].photograph;
-    catPic.setAttribute("alt", "Blurry tabby cat with pink nose");
-    catPic.addEventListener("click", addClick, false);
+    this.renderCat();
+  };
 
-// let catPic2 = document.getElementById("cat-pic2");
-    // catPic2.setAttribute("src", "img/trouble-one.jpg");
-    // catPic2.setAttribute("alt", "white cat resting");
-    // catPic2.addEventListener("click", function()  {
-      // clicks2++;
-      // counter2.innerHTML = clicks2;
-    // }, false);
+  function renderCat() {
+    var currentCat = getCurrentCat();
+    this.counter.textContent = currentCat.clickCount + " " + currentCat.name + " Clicks";
+    this.catName.textContent = currentCat.name;
+    this.catPic.src = currentCat.photograph;
+    this.catPic.setAttribute("alt", currentCat.description);
+  };
 
-let counter = document.getElementById("counter");
-let clicks = 0;
-
-function addClick() {
-  clicks++;
-  counter.innerHTML = clicks;
-};
- */
+initOctopus();
